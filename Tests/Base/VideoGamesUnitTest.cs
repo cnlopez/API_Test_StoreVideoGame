b7490a366a1c18e8be8e3589339cbba3678046bd
cnlopez <cnlopez1@hotmail.com>
2023-05-21 15:30:55 -0500

    Unit tests

---
+++ Tests/Base/VideoGamesUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Base
+{
+    public abstract partial class VideoGamesUnitTest
+    {
+        protected IGameService GameService { get; set; }
+    }
+}
---
+++ Tests/GetVideoGamesMapperTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Base;
+
+namespace Tests
+{
+    public class GetVideoGamesMapperTest : VideoGamesUnitTest
+    {
+        [Test]
+        public async Task GetGetVideoGamesMapper_ReturnSuccess()
+        {
+            var response = await GameService.GetVideoGamesMapper();
+            Assert.IsNotNull(response);
+        }
+    }
+}
