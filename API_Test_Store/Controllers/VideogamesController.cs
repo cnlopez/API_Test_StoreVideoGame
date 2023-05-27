7178ef1a09b7e849d8449e7049fd731b5c9be718
cnlopez <cnlopez1@hotmail.com>
2023-05-27 11:28:40 -0500

    Comments

--- API_Test_Store/Controllers/VideogamesController.cs
+++ API_Test_Store/Controllers/VideogamesController.cs
@@ -10,32 +10,39 @@ using ViewModels.Game;
 
 namespace API_Test_Store.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
     public class VideogamesController : ControllerBase
     {
         private readonly IGameService _gameService;
-        public VideogamesController(IGameService gameService)
-        {
-            _gameService = gameService;
-        }
+        public VideogamesController(IGameService gameService) => _gameService = gameService;
 
-        // GET: api/Videogames
-        [HttpGet("video-games")]
+        /// <summary>
+        /// Get video game
+        /// </summary>
+        /// <returns></returns>
+        [Route("video-games"), HttpGet]
         public async Task<ActionResult> GetVideogamesMapper()
         {
             return Ok(await _gameService.GetVideoGamesMapper());
         }
 
-        // GET: api/Videogames
-        [HttpGet("video-games/{videoGameId:int}")]
+        /// <summary>
+        /// Get Video Games
+        /// </summary>
+        /// <param name="videoGameId"></param>
+        /// <returns></returns>
+        [Route("video-games/{videoGameId:int}"), HttpGet]
         public async Task<ActionResult> GetVideogamesMapper(int videoGameId)
         {
             return Ok(await _gameService.GetVideoGamesMapper(videoGameId));
         }
 
-        // POST: api/Videogames
-        [HttpPost("video-games")]
+        /// <summary>
+        /// Save Video game
+        /// </summary>
+        /// <param name="videoGame"></param>
+        /// <returns></returns>
+        [Route("video-games"), HttpPost]
         public async Task<ActionResult> SaveVideogamesMapper(VideogamesViewModel videoGame)
         {
             return Ok(await _gameService.SaveVideoGamesMapper(videoGame));
@@ -47,10 +54,13 @@ namespace API_Test_Store.Controllers
         /// <param name="videoGameId"></param>
         /// <param name="videoGame"></param>
         /// <returns></returns>
-        [HttpPut("video-games/{videoGameId:int}")]
+        [Route("video-games/{videoGameId:int}"), HttpPut]
         public async Task<ActionResult> UpdateVideogamesMapper(int videoGameId, VideogamesViewModel videoGame)
         {
             return Ok(await _gameService.SaveVideoGamesMapper(videoGame));
         }
+
+
+        //Create Delete
     }
 }
