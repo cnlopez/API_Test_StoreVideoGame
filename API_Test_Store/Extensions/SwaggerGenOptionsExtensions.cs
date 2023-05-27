07414eb1d5d2728949db20eed33d6a6d056f9059
ceos10 <ocrospoma.c.e@gmail.com>
2023-05-27 11:17:48 -0500

    Adding Swagger extension

---
+++ API_Test_Store/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
+namespace API_Test_Store.Extensions;
+
+public static class SwaggerGenOptionsExtensions
+{
+    /// <summary>
+    /// Set the comments path for the Swagger JSON and UI.
+    /// Xml documentation needs to be enabled in the project.
+    /// </summary>
+    /// <param name="swaggerGenOptions"></param>
+    public static void IncludeLocalXmlComments(this SwaggerGenOptions swaggerGenOptions)
+    {
+        var xmlFile = $"{Assembly.GetEntryAssembly()!.GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        swaggerGenOptions.IncludeXmlComments(xmlPath);
+    }
+}
--- API_Test_Store/Program.cs
+++ API_Test_Store/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Data;
 using Services;
+using API_Test_Store.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<API_Test_StoreContext>(options =>
@@ -12,7 +13,10 @@ builder.Services.RegisterBusiness(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.IncludeLocalXmlComments();
+});
 
 var app = builder.Build();
 
